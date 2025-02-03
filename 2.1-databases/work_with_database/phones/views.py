11031bc94adcf0f88449914e1e4b8ed5b9c26137
ilya-1001 <ikobtsev83@yandex.ru>
2025-02-04 01:51:09 +0300

    Update views.py

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,22 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all()
+    sort_pages = request.GET.get('sort')
+
+    if sort_pages == 'name':
+        phones = phones.order_by('name')
+    elif sort_pages == 'min_price':
+        phones = phones.order_by('price')
+    elif sort_pages == 'max_price':
+        phones = phones.order_by('-price')
+
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
