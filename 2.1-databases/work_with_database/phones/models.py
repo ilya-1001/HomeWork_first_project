f2ca41c721a7585cafc3e0893388d9c827557489
ilya-1001 <ikobtsev83@yandex.ru>
2025-02-04 01:48:54 +0300

    Update models.py

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=100)
+    image = models.URLField()
+    price = models.IntegerField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(unique=True)
