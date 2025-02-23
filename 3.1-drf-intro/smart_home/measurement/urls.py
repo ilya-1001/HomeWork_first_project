143bd89ea5a871c6651e4013b673c90b0bdb1f21
ilya-1001 <ikobtsev83@yandex.ru>
2025-02-23 20:27:52 +0300

    Update urls.py

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from measurement.views import ListSensorsSerializerView, CreateSensorsView, \
+    ListSensorDetailView, MeasurementView, UpdateSensorView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('createsensors/', CreateSensorsView.as_view()),
+    path('sensors/', ListSensorsSerializerView.as_view()),
+    path('sensor/<pk>/', ListSensorDetailView.as_view()),
+    path('measurements/', MeasurementView.as_view()),
 ]
